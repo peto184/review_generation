e5ec9b16c48cf30c68feb6bce397f822c5f93e10
Peter Papay <peto184@gmail.com>
2019-02-12 20:08:43 +0100

    char level implementation

--- data.py
+++ data.py
@@ -5,44 +5,63 @@ from io import open
 from util import load_data
 
 class Dictionary(object):
+    # Word level or character level
     def __init__(self):
-        self.word2idx = {}
-        self.idx2word = []
+        self.element2idx = {}
+        self.idx2element = []
 
-    def add_word(self, word):
-        if word not in self.word2idx:
-            self.idx2word.append(word)
-            self.word2idx[word] = len(self.idx2word) - 1
-        return self.word2idx[word]
+    def add_element(self, word):
+        if word not in self.element2idx:
+            self.idx2element.append(word)
+            self.element2idx[word] = len(self.idx2element) - 1
+        return self.element2idx[word]
     
     def __len__(self):
-        return len(self.idx2word)
+        return len(self.idx2element)
 
 class Corpus(object):
     def __init__(self, args):
         self.dictionary = Dictionary()
-        self.train = self.tokenize(os.path.join(args.data, 'train.json'))
-        self.valid = self.tokenize(os.path.join(args.data, 'valid.json'))
 
-    def tokenize(self, path):
+        self.use_char_embeds = args.type == 'char' 
+        
+        self.train = self.tokenize(os.path.join(args.data, 'train.json'), use_char_embeds=self.use_char_embeds)
+        self.valid = self.tokenize(os.path.join(args.data, 'valid.json'), use_char_embeds=self.use_char_embeds)
+
+    def tokenize(self, path, use_char_embeds=False):
         assert os.path.exists(path)
 
         data = load_data(path)
         data = ' '.join(data['reviewText'])
 
-        # Add words to the dictionary
+        
         tokens = 0
-        words = data.split() + ['<eos>']    
+        if use_char_embeds:
+            data = list(data)
+            words = data + ['<eos>']
+        else :
+             words = data.split() + ['<eos>']
+
+        # Add words to the dictionary
         tokens += len(words)
         for word in words:
-            self.dictionary.add_word(word)
+            self.dictionary.add_element(word)
 
         # Tokenbize file content
         ids = torch.LongTensor(tokens)
         token = 0
 
         for word in words:
-                ids[token] = self.dictionary.word2idx[word]
+                ids[token] = self.dictionary.element2idx[word]
                 token += 1
         
         return ids
+
+    def describe(self):
+
+        embed_type = 'char' if self.use_char_embeds else 'word'
+
+        s = f'Using {embed_type} level embeddings\n'
+        s += f'Size of dictionary {len(self.dictionary)} \n'
+        
+        return s
